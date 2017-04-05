46d2acc00d476d5515e206d12289cbacd3f6732a
caogen <774750747@qq.com>
2017-04-05 20:16:50 +0800

    微信开发学习

--- src/main/java/com/caogen/WeiXinStudy/controller/WeiXinController.java
+++ src/main/java/com/caogen/WeiXinStudy/controller/WeiXinController.java
@@ -1,8 +1,15 @@
 package com.caogen.WeiXinStudy.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.caogen.WeiXinStudy.util.SignUtil;
 
 
 @Controller
@@ -11,10 +18,37 @@ public class WeiXinController {
 
 	private static Logger logger = Logger.getLogger(WeiXinController.class); 
 	
-	@RequestMapping("/helloworld")
-	public String sayHello() {
-		logger.info("Hello World!");
-		System.out.println("Hello World!");
-		return "weixin";
+	/**
+	 * 验证服务器地址的有效性
+	 * @param request
+	 * @param response
+	 * @author 草根
+	 */
+	@RequestMapping("/checkWeiXin")
+	public void checkWeiXin(HttpServletRequest request, HttpServletResponse response) {
+		//微信加密签名
+		String signature = request.getParameter("signature");
+		
+		//时间戳
+		String timestamp = request.getParameter("timestamp");
+		
+		//随机数
+		String nonce = request.getParameter("nonce");
+		
+		//随机字符串
+		String echostr = request.getParameter("echostr");
+		
+		logger.info("检验signature开始");
+		if(SignUtil.checkSignature(signature, timestamp, nonce)){
+			logger.info("返回echostr");
+			try {
+				response.getWriter().print(echostr);
+			} catch (IOException e) {
+				logger.info("返回echostr失败！");
+				e.printStackTrace();
+			}
+		}
+		
+		logger.info("检验signature结束");
 	}
 }
