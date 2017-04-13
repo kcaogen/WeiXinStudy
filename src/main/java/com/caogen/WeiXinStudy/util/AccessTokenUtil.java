21580e0bcdb993288868ac06e3b0c48232ee667a
caogen <774750747@qq.com>
2017-04-13 09:18:14 +0800

    微信开发学习

--- src/main/java/com/caogen/WeiXinStudy/util/AccessTokenUtil.java
+++ src/main/java/com/caogen/WeiXinStudy/util/AccessTokenUtil.java
@@ -1,5 +1,8 @@
 package com.caogen.WeiXinStudy.util;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.apache.log4j.Logger;
 import org.springframework.util.StringUtils;
 
@@ -29,40 +32,73 @@ public class AccessTokenUtil {
         	return access_token;
         }
 		
-		// 访问微信服务器
-		String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appID + "&secret="
-				+ appsecret;
-		String message = HttpUtil.sendGet(url);
-		
-		if(StringUtils.isEmpty(message)){
-			return null;
-		}
-		
-		JSONObject json = JSONObject.parseObject(message);
-
-		if(json.get("errcode") != null){
-			logger.info("获取access_token返回错误码:" + json.get("errcode").toString());
-			return null;
-		}
-		access_token = json.get(ACCESSTOKEN).toString();
-		
 		/**
-		 * access_token的有效期目前为2个小时，需定时刷新，重复获取将导致上次获取的access_token失效。
-		 * 虽然有效期是两个小时，但是缓存有效时间还是少于两个小时更好点，以防网络延迟等事故
+		 * 以防多个线程同时调用这个方法并且redis里面没有数据
+		 * 这个时候只能有一个线程通过微信服务器获取access_token
+		 * 当第一个线程执行完这些同步代码块之后，redis有数据了
+		 * 第二个线程进来同步代码块之后就不会再去微信服务器获取access_token
 		 */
-		jedis.setex(ACCESSTOKEN, Integer.parseInt(json.get("expires_in").toString())-100, access_token);
-		
+		int expires_in;
+		synchronized (ACCESSTOKEN) {
+			access_token = jedis.get(ACCESSTOKEN);
+			if(!StringUtils.isEmpty(access_token)){
+				RedisPool.close(jedis);
+	        	return access_token;
+	        }
+			
+			// 访问微信服务器
+			String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appID + "&secret="
+					+ appsecret;
+			String message = HttpUtil.sendGet(url);
+			
+			if(StringUtils.isEmpty(message)){
+				return null;
+			}
+			
+			JSONObject json = JSONObject.parseObject(message);
+
+			if(json.get("errcode") != null){
+				logger.info("获取access_token返回错误码:" + json.get("errcode").toString());
+				return null;
+			}
+			access_token = json.get(ACCESSTOKEN).toString();
+			expires_in = Integer.parseInt(json.get("expires_in").toString())-100;
+			
+			/**
+			 * access_token的有效期目前为2个小时，需定时刷新，重复获取将导致上次获取的access_token失效。
+			 * 虽然有效期是两个小时，但是缓存有效时间还是少于两个小时更好点，以防网络延迟等事故
+			 */
+			jedis.setex(ACCESSTOKEN, expires_in, access_token);
+			
+		}
 		RedisPool.close(jedis);
 		
 		return access_token;
 	}
 	
 	public static void main(String[] args) {
-		try {
+		/*try {
 			System.out.println(getAccessToken());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}*/
+		
+		ExecutorService exec = Executors.newCachedThreadPool();
+		for(int i = 0; i < 4; i++){
+			exec.execute(new Runnable() {
+				
+				@Override
+				public void run() {
+					try {
+						System.out.println(getAccessToken());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
 		}
+		exec.shutdown();
+		
 	}
 }
